596a153f53b543766b12c744bd13d09874f96770
niku948 <84850740+niku948@users.noreply.github.com>
2021-05-29 09:48:01 +0530

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,30 +1,4 @@
-/*
-async function getUsers(){
-    let response = await fetch('www.example.com/api/get/1');
-    let data = await response.json();
-    return data;
-}
-getUsers().then(data => console.log(data));
-
-document.write("hi");
-
-*/
-/*
-const userAction = async () => {
-    const response = await fetch('http://example.com/movies.json', {
-      method: 'POST',
-      body: myBody, // string or object
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const myJson = await response.json(); //extract JSON from the http response
-    // do something with myJson
-
-    document.getElementById('result').innerHTML = response.myJson;
-  }
-  */
-  const userAction = async () => {
+ const userAction = async () => {
     const response = await fetch('http://example.com/movies.json', {
       method: 'POST',
       body: myBody, // string or object
